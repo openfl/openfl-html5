2343f6157b28a242027f62921302468518ebee74
Joshua Granick <joshua@joshuagranick.com>
2014-03-07 12:02:29 -0800

    Add missing files

---
+++ flash/display/IBitmapDrawable.hx
@@ -0,0 +1,11 @@
+package flash.display;
+
+
+import flash.display.Stage;
+
+
+interface IBitmapDrawable {
+	
+	function __renderCanvas (renderSession:RenderSession):Void;
+	
+}
---
+++ flash/geom/Transform.hx
@@ -0,0 +1,24 @@
+package flash.geom;
+
+
+import flash.display.DisplayObject;
+
+
+class Transform {
+	
+	
+	public var colorTransform:ColorTransform;
+	public var concatenatedColorTransform:ColorTransform;
+	public var concatenatedMatrix:Matrix;
+	public var matrix:Matrix;
+	public var pixelBounds:Rectangle;
+	
+	
+	public function new (displayObject:DisplayObject) {
+		
+		
+		
+	}
+	
+	
+}
